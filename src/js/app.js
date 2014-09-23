c7e2e62126e1c8b68dae0bec41ad42b6d80a6e5d
onaliugo <contact@onaliugo.fr>
2014-09-23 17:32:02 +0200

    Animate slides

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,17 @@
 var slides = {
   $el: document.querySelectorAll('.slide'),
   elCounter: 0,
-  isBusy: true
+  isBusy: true,
+
+  array: [],
+
+  // slides
+  // {$el: $el, animationIn: 'fadeIn'};
+  current: null,
+  next: null,
+
+  // direction 'next'|'prev'
+  dir: null
 };
 
 var Slides = function(opts){
@@ -12,18 +22,46 @@ var Slides = function(opts){
 
 Slides.prototype = slides;
 Slides.prototype.setCurrent = function(){
-  this.$el[this.elCounter ].classList.add('is-current');
+  this.$el[this.elCounter].classList.add('is-current');
 };
 Slides.prototype.init = function(){
-
   // count total slides
   this.elLength = this.$el.length;
 
   // set current slide
   this.setCurrent();
 
+  // create an object for each slide
+  // and push it to a main array
+  [].forEach.call(
+    this.$el,
+    function($el, i){
+      var obj = {
+        $el: $el,
+        animationIn: $el.dataset.animation
+      };
+      slides.array.push(obj);
+    }
+  );
+
   // ready
   this.isBusy = false;
+};
+Slides.prototype.animSlides = function(dir){
+  // targets
+  this.current = this.array[this.elCounter];
+  this.next = this.array[ this.currentDir === "next" ? this.elCounter + 1 : this.elCounter - 1 ];
+
+  // current slide
+  this.current.$el
+    .classList.add('fadeOut');
+
+  // next slide
+  this.next.$el
+    .classList.add(this.next.animationIn);
+
+  // callback
+  // do stuff
 };
 
 // init slides
@@ -35,16 +73,16 @@ document.onkeydown = function(e){
   var keyCode = e.keyCode;
 
   if(keyCode === 39){
-    dir = 'next';
+    slides.currentDir = 'next';
     canAnim = (slides.elCounter < slides.elLength - 1);
   }
   else if(keyCode === 37){
-    dir = 'prev';
+    slides.currentDir = 'prev';
     canAnim = (slides.elCounter > 0);
   }
 
-  if(canAnim && !this.isBusy){
+  if(canAnim && !slides.isBusy){
     slides.isBusy = true;
-    // this.animSlides(dir);
+    slides.animSlides();
   }
 };
